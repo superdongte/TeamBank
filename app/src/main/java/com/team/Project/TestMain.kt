96df017a3ef68447c87d71b49282d85a6c7a07c1
ryujaehyeon <rjh7465@naver.com>
2021-12-21 13:48:21 +0900

    화면 설계 틀 구현

---
+++ app/src/main/java/com/team/Project/TestMain.kt
@@ -0,0 +1,24 @@
+package com.team.Project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+import android.widget.GridView
+import android.view.View
+import android.widget.ImageView
+
+
+class TestMain : AppCompatActivity() {
+//    var age = arrayOf(
+//        "10대\n미만", "20대", "30대",
+//        "40대","50대", "60대",
+//        "뒤로가기","70대\n이상"
+//    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.choosegoods)
+
+
+    }
+}
