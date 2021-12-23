b7307920675c395f822aa15bc061206baaf48033
tosery <83147935+tosery@users.noreply.github.com>
2021-12-24 00:48:14 +0900

    [준영] AgeAdapter 클릭리스너 추가(2번화면 ->3번화면) 완료

--- app/src/main/java/com/team/project/AgeAdapter.kt
+++ app/src/main/java/com/team/project/AgeAdapter.kt
@@ -1,20 +1,22 @@
 package com.team.project
 
 import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class AgeAdapter(
     private val items: ArrayList<AgeModel>,
-    var activity: Activity,
+    var activity: Activity
 ) : RecyclerView.Adapter<AgeAdapter.AgeViewHolder>(){
     var row_index = -1
-//    var check = true
+    //    var check = true
 //    var select = true
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AgeViewHolder {
         val view =
@@ -25,19 +27,27 @@ class AgeAdapter(
     override fun onBindViewHolder(holder: AgeViewHolder, position: Int) {
         var currentItem = items[position]
         holder.imageView.setImageResource(currentItem.image)
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView?.context, ChooseGoodsActivity::class.java)
+            ContextCompat.startActivity(holder.imageView.context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    class AgeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class AgeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         var imageView: ImageView
         var gridLayout: GridLayout
 
         init{
             imageView = itemView.findViewById(R.id.thumbnailIamgeView2)
             gridLayout = itemView.findViewById(R.id.linearLayout2)
+            itemView.setOnClickListener{
+                //TODO: 토스트테스트해보기
+                //클릭 애니매이션 해보기
+            }
         }
     }
 
