5d60573ae98ab9e9352c9265cc508b4df4c06ecc
tosery <83147935+tosery@users.noreply.github.com>
2021-12-23 14:29:27 +0900

    [준영] 상품 추천화면 틀 만들기(xml)

---
+++ app/src/main/java/com/team/project/ChooseGoodsActivity.kt
@@ -0,0 +1,11 @@
+package com.team.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseGoodsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_goods)
+    }
+}
