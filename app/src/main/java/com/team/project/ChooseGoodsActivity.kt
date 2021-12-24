de958dcc7ae28ea1157cd3a7683d3127d8330ad4
tosery <83147935+tosery@users.noreply.github.com>
2021-12-24 13:22:20 +0900

    [준영]상품화면 Log.i추가

--- app/src/main/java/com/team/project/ChooseGoodsActivity.kt
+++ app/src/main/java/com/team/project/ChooseGoodsActivity.kt
@@ -2,10 +2,20 @@ package com.team.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.team.project.databinding.ActivityChooseGoodsBinding
 
 class ChooseGoodsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityChooseGoodsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_goods)
+        Log.d("MyTag","ChooseGoodsActivity onCreate()")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_choose_goods)
+
+        var temp = intent.getStringExtra("genage")
+        temp?.let{Log.i("MyTag", it)}
+
     }
 }
