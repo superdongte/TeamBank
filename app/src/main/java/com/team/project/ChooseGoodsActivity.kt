bb246ac6fd6a471083389ae5323851a9e2762702
tosery <83147935+tosery@users.noreply.github.com>
2021-12-27 22:02:22 +0900

    [준영] 연결하기

--- app/src/main/java/com/team/project/ChooseGoodsActivity.kt
+++ app/src/main/java/com/team/project/ChooseGoodsActivity.kt
@@ -1,8 +1,10 @@
 package com.team.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.team.project.databinding.ActivityChooseGoodsBinding
 
@@ -16,6 +18,22 @@ class ChooseGoodsActivity : AppCompatActivity() {
 
         var temp = intent.getStringExtra("genage")
         temp?.let{Log.i("MyTag", it)}
+
+        binding.goodsPic1.setOnClickListener(View.OnClickListener {
+            Log.d("MyTag", "click goodsPic1")
+            val intent = Intent(this, InstallmentActivity::class.java)
+            startActivity(intent)
+        })
+        binding.goodsPic2.setOnClickListener(View.OnClickListener {
+            Log.d("MyTag", "click goodsPic1")
+            val intent = Intent(this, InstallmentActivity::class.java)
+            startActivity(intent)
+        })
+        binding.goodsPic3.setOnClickListener(View.OnClickListener {
+            Log.d("MyTag", "click goodsPic1")
+            val intent = Intent(this, InstallmentActivity::class.java)
+            startActivity(intent)
+        })
 
     }
 }
--- app/src/main/java/com/team/project/InstallmentActivity.kt
+++ app/src/main/java/com/team/project/InstallmentActivity.kt
@@ -1,11 +1,24 @@
 package com.team.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.team.project.databinding.ActivityInstallmentBinding
 
 class InstallmentActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityInstallmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_installment)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_installment)
+
+        binding.textView1.setOnClickListener(View.OnClickListener {
+            Log.d("MyTag","click textView1")
+            val intent = Intent(this, MapDetailsActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
