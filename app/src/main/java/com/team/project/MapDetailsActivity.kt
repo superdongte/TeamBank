7d4e331ae77063df05eb7ab3310ff292e49ed8ce
ryujaehyeon <rjh7465@naver.com>
2021-12-31 16:44:16 +0900

    현재위치에서 가까운 지점은행 찾아서 지도에 나타내기 완료

--- app/src/main/java/com/team/project/MapDetailsActivity.kt
+++ app/src/main/java/com/team/project/MapDetailsActivity.kt
@@ -1,11 +1,23 @@
 package com.team.project
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -21,10 +33,19 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.util.*
 
 //OnMapReadyCallbak은 인스턴스를 얻기 위해 사용되는 콜백 인터페이스
 //오버레이 클릭에 대한 이벤트 리스너 인터페이스.
 class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnClickListener {
+    //Gps 구현부분
+    private var gpsTracker: GpsTracker? = null
+    var REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
@@ -62,8 +83,14 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_details)
-        mapView.onCreate(savedInstanceState)
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting()
+        } else {
+            checkRunTimePermission()
+        }
 
+
+        mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
         viewPager.adapter = viewPagerAdapter
@@ -71,7 +98,6 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
 
@@ -81,7 +107,6 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
 
                 naverMap.moveCamera(cameraUpdate)
             }
-
         })
         Log.d("MapDetailsActivity", "테스트...")
     }
@@ -91,8 +116,11 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
         naverMap.maxZoom = 18.0
         naverMap.minZoom = 10.0
 
+        gpsTracker = GpsTracker(this@MapDetailsActivity)
+        val mylat = gpsTracker!!.getLatitude()
+        val mylng = gpsTracker!!.getLongitude()
         //카메라 업데이트에서 위도 경도를 입력해서 초기화면 지정
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.24666969931327, 129.08864774272868))
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(mylat, mylng))
         naverMap.moveCamera(cameraUpdate)
 
         val uiSetting = naverMap.uiSettings
@@ -121,11 +149,11 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
                         }
 
                         response.body()?.let { dto ->
-                            updateMarker(dto.items)
                             viewPagerAdapter.submitList(dto.items)
                             recyclerAdapter.submitList(dto.items)
 
                             bottomSheetTitleTextView.text = "${dto.items.size}개의 지점"
+                            updateMarker(dto.items)
                         }
                     }
 
@@ -139,7 +167,29 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
     }
 //마커 설정
     private fun updateMarker(banks: List<BankModel>) {
+        gpsTracker = GpsTracker(this@MapDetailsActivity)
+        val mylat = gpsTracker!!.getLatitude()
+        val mylng = gpsTracker!!.getLongitude()
+        Toast.makeText(applicationContext,"현재위치 \n위도:$mylat , \n경도:$mylng",Toast.LENGTH_LONG).show()
+        var closeBank: BankModel = BankModel(0, "", "", 0.0, 0.0, "")
+        var oldDistance:Float = 0.0F
         banks.forEach { bank ->
+            //거리구하기
+            val mylocation = Location("mylocation")
+            mylocation.setLatitude(mylat)
+            mylocation.setLongitude(mylng)
+            val banklocation = Location("banklocation")
+            banklocation.setLatitude(bank.lat)
+            banklocation.setLongitude(bank.lng)
+            var distance = mylocation.distanceTo(banklocation)
+            Log.d(TAG, "Distance: $distance")
+            if (distance < oldDistance){
+                oldDistance = distance
+                closeBank = bank
+            }else if (oldDistance == 0.0F){
+                oldDistance = distance
+                closeBank = bank
+            }
             val marker = Marker()
             marker.position = LatLng(bank.lat, bank.lng)
             marker.onClickListener = this
@@ -148,6 +198,14 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
             marker.tag = bank.id
             marker.icon = MarkerIcons.BLACK
             marker.iconTintColor = Color.rgb(255,74,74)
+        }
+
+        var list = viewPagerAdapter.currentList
+        for (i: Int in 0..list.size){
+            if (list[i] == closeBank) {
+                viewPager.currentItem = i
+                break
+            }
         }
     }
 
@@ -163,6 +221,47 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
             return
+        }
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grantResults.size == REQUIRED_PERMISSIONS.size) {
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            var check_result = true
+
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false
+                    break
+                }
+            }
+            if (check_result) {
+
+                //위치 값을 가져올 수 있음
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[0]
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[1]
+                    )
+                ) {
+                    Toast.makeText(
+                        this@MapDetailsActivity,
+                        "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this@MapDetailsActivity,
+                        "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
         }
 
     }
@@ -203,6 +302,8 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
     }
 
     companion object {
+        private const val GPS_ENABLE_REQUEST_CODE = 2001
+        private const val PERMISSIONS_REQUEST_CODE = 100
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 
@@ -219,4 +320,97 @@ class MapDetailsActivity : AppCompatActivity(), OnMapReadyCallback, Overlay.OnCl
         return true
     }
 
+    fun checkRunTimePermission() {
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this@MapDetailsActivity,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this@MapDetailsActivity,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MapDetailsActivity,
+                    REQUIRED_PERMISSIONS[0]
+                )
+            ) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(this@MapDetailsActivity, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG)
+                    .show()
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this@MapDetailsActivity, REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE
+                )
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this@MapDetailsActivity, REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+        }
+    }
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private fun showDialogForLocationServiceSetting() {
+        val builder = AlertDialog.Builder(this@MapDetailsActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage(
+            """
+                앱을 사용하기 위해서는 위치 서비스가 필요합니다.
+                위치 설정을 수정하실래요?
+                """.trimIndent()
+        )
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정") { dialog, id ->
+            val callGPSSettingIntent =
+                Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(
+                callGPSSettingIntent,
+                GPS_ENABLE_REQUEST_CODE
+            )
+        }
+        builder.setNegativeButton(
+            "취소"
+        ) { dialog, id -> dialog.cancel() }
+        builder.create().show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음")
+                        checkRunTimePermission()
+                        return
+                    }
+                }
+        }
+    }
+
+    fun checkLocationServicesStatus(): Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
 }
