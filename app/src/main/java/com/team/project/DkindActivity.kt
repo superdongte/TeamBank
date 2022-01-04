33bd4be2300f65192f08b17ae42eb7bdb1a2061a
ryujaehyeon <rjh7465@naver.com>
2022-01-04 23:24:02 +0900

    gif넣기

--- app/src/main/java/com/team/project/DkindActivity.kt
+++ app/src/main/java/com/team/project/DkindActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.team.project.databinding.ActivityChooseDkindBinding
 
 class DkindActivity : AppCompatActivity() {
@@ -15,12 +17,19 @@ class DkindActivity : AppCompatActivity() {
     private val webView: WebView by lazy{
         findViewById(R.id.dkwebview1)
     }
+//    private val webView2: WebView by lazy{
+//        findViewById(R.id.gifimage2)
+//    }
 
     private fun initViews(){
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         var url = "http://10.0.2.2:8083"
         webView.loadUrl(url)
+//        webView2.webViewClient = WebViewClient()
+//        webView2.settings.javaScriptEnabled = true
+//        var url2 = "http://10.0.2.2:8083"
+//        webView2.loadUrl(url2)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,5 +53,7 @@ class DkindActivity : AppCompatActivity() {
             startActivity(intent)
         })
         initViews()
+        var imageView2 = findViewById<ImageView>(R.id.gifimage2)
+        Glide.with(this).load(R.raw.gif3).into(imageView2);
     }
 }
